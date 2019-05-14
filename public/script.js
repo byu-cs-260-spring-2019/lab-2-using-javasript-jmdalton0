94919f7a7d1c21ad10fa92de9569bfa424aee878
jmdalton0 <jmdalton0@gmail.com>
2019-05-13 22:27:58 -0600

    https

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ window.onload = function () {
 
         const key = "38ac84dd240729ceb6d8583b6d2e7f8e"; //my API key for Open Weather Map
 
-        const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=" + key;
+        const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=" + key;
         fetch(weatherUrl)
             .then(function (response) {
                 return response.json();
@@ -19,7 +19,7 @@ window.onload = function () {
 
                 let results = "";
                 for (let i = 0; i < json.weather.length; i++) {
-                    results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+                    results += '<img src="https://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
                 }
                 results += '<h3>' + json.main.temp + " &deg;F</h3>";
                 results += '<p>high ' + json.main.temp_max + " &deg;F</p>";
@@ -39,7 +39,7 @@ window.onload = function () {
                 document.getElementById("weatherResults").innerHTML = results;
             });
 
-        forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ",US&units=imperial" + "&APPID=" + key;
+        forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + value + ",US&units=imperial" + "&APPID=" + key;
         fetch(forecastUrl)
             .then(function (response) {
                 return response.json();
