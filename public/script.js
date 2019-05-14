f7eb6f6579d2db0f8a893b89cebefe366fcd40f0
jmdalton0 <jmdalton0@gmail.com>
2019-05-13 22:15:34 -0600

    firebase init

--- public/script.js
+++ public/script.js
@@ -48,11 +48,51 @@ window.onload = function () {
 
                 let results = "";
                 for (let i = 0; i < json.list.length; i++) {
-                    
+
+                    /*row*/
+                    if (i % 8 === 0) {
+                        var date = new Date();
+                        var day = date.getDay() + i / 8;
+                        console.log(day);
+                        var dayOfWeek = getDayOfWeek(day);
+                        results += '<h3>' + dayOfWeek + "</h3>";
+                        results += '<div class="row">';
+                    }
+
+                    /*box*/
+                    results += '<div class="box">';
+                    results += '<p>' + json.list[i].main.temp + " &deg;F</p";
+                    results += '</div>';
+
+                    /*end row*/
+                    if (i % 8 === 7) {
+                        results += '</div>';
+                    }
                 }
 
                 document.getElementById("3hourResults").innerHTML = results;
             });
     });
+
+    const getDayOfWeek = function (day) {
+        var answer;
+        switch (day) {
+            case 1: answer = "Monday";
+                break;
+            case 2: answer = "Tuesday";
+                break;
+            case 3: answer = "Wednesday";
+                break;
+            case 4: answer = "Thursday";
+                break;
+            case 5: answer = "Friday";
+                break;
+            case 6: answer = "Saturday";
+                break;
+            default: answer = "Sunday";
+                break;
+        }
+        return answer;
+    }
 
 }
